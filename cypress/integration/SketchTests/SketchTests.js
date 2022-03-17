f492c0fa1cae53f383f4bb2f48f2b172cf9151d2
Miguel Contte <mcontte@qubeyond.com>
2022-03-17 17:38:05 -0300

    Added User Does Not Exist , Forgot Password tests

--- cypress/integration/SketchTests/SketchTests.js
+++ cypress/integration/SketchTests/SketchTests.js
@@ -9,7 +9,7 @@ describe('Sketch Page test', () =>{
 
         cy.get("#text-input").should('be.visible').type(Cypress.env('user_name')).should('have.value', Cypress.env('user_name'))
 
-        cy.get('[data-testid="input"]').type(Cypress.env('user_password'), {log: false})
+        cy.get('[data-testid="input"]').should('be.visible').type(Cypress.env('user_password'), {log: false})
 
         cy.get(".sc-jWa-DWe > .sc-hiwReK").click()
 
@@ -27,7 +27,7 @@ describe('Sketch Page test', () =>{
         // We use a username with a bad syntax to force an error
 
         cy.get("#text-input").should('be.visible').type('Bad Username')
-        cy.get('[data-testid="input"]').type(Cypress.env('user_password'), {log: false})
+        cy.get('[data-testid="input"]').should('be.visible').type(Cypress.env('user_password'), {log: false})
 
         cy.get(".sc-jftGvU").should('be.visible') // check the message error is shown
 
@@ -41,7 +41,7 @@ describe('Sketch Page test', () =>{
 
         // We use an incorrect password to force an error
 
-        cy.get('[data-testid="input"]').type('PasswordIncorrect')
+        cy.get('[data-testid="input"]').should('be.visible').type('PasswordIncorrect')
 
         cy.get(".sc-jWa-DWe > .sc-hiwReK").click()
 
@@ -50,4 +50,33 @@ describe('Sketch Page test', () =>{
         cy.wait(2500) // shows the bad password error message and quits  
     })
 
+    it ('User does not exist', () => {
+        cy.visit('https://www.sketch.com/signin')
+
+        cy.get("#text-input").should('be.visible').type(Cypress.env('user_name')).should('have.value', Cypress.env('user_name'))
+
+        // We use an incorrect password to force an error
+
+        cy.get('[data-testid="input"]').should('be.visible').type('PasswordIncorrect')
+
+        cy.get(".sc-jWa-DWe > .sc-hiwReK").click()
+
+        cy.get(".sc-jftGvU").should('be.visible') // check if the error message is visible
+  
+        cy.wait(2500) // shows the bad password error message and quits  
+    })
+
+    it ('Forgot Password?', () => {
+        cy.visit('https://www.sketch.com/signin')
+
+        cy.get(".sc-furvIG > .sc-bTfYlY").click()
+
+        cy.get("#email-input").should('be.visible').type(Cypress.env('user_name')).should('have.value', Cypress.env('user_name'))
+
+        cy.get(".sc-jWa-DWe > .sc-hiwReK").click() // Reset Password button
+
+        cy.get('.sc-iutpoz > .sc-hiwReK').click() // Go back to Sign in
+
+        cy.wait(2500) // shows the bad password error message and quits  
+    })
 })
