3c69a597b84b2f02e42af8d4f5de17bfb0f76a79
Miguel Contte <mcontte@qubeyond.com>
2022-03-17 16:03:58 -0300

    Version 0.1

---
+++ cypress/integration/SketchTests/SketchTests.js
@@ -0,0 +1,40 @@
+// SketchTests.js created with Cypress
+//
+// Start writing your Cypress tests below!
+// If you're unfamiliar with how Cypress works,
+// check out the link below and learn how to write your first test:
+// https://on.cypress.io/writing-first-test
+
+describe('Sketch Page test', () =>{
+    it ('Happy Path login', () => {
+        cy.visit('https://www.sketch.com/signin')
+
+        // set username and password in the cypress.env.json file
+
+        cy.get("#text-input").should('be.visible').type(Cypress.env('user_name')).should('have.value', Cypress.env('user_name'))
+
+        cy.get('[data-testid="input"]').type(Cypress.env('user_password'), {log: false})
+
+        cy.get(".sc-jWa-DWe > .sc-hiwReK").click()
+
+        // "Get Started for Free" popup
+
+        cy.get(".sc-fXMSbX > :nth-child(1) > .sc-hiwReK" , { timeout: 10000 }).click() // the timeout is to ensure the popup 
+                                                                                       // is deployed
+
+        cy.wait(5000) // show the main user screen and quits
+    })
+
+    it ('Bad username login', () => {
+        cy.visit('https://www.sketch.com/signin')
+
+        // We use a username with a bad syntax to force an error
+
+        cy.get("#text-input").should('be.visible').type('Bad Username')
+
+        cy.get('[data-testid="input"]').type(Cypress.env('user_password'), {log: false})
+
+        cy.get(".sc-jWa-DWe > .sc-hiwReK").click()
+
+    })
+})
